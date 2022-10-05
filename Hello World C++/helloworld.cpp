6326215a16cfc2d5db302c382bc4b11b63035dea
Ivan Junior <57573456+Ivanssjunior@users.noreply.github.com>
2022-10-06 05:07:47 +0900

    Files and Studies from the Module Hello World C++

---
+++ Hello World C++/helloworld.cpp
@@ -0,0 +1,24 @@
+// Includes the iostream library
+#include <iostream>
+// i => input
+// o => output
+
+using namespace std;
+// std => standart 
+// It means the cpp will just use what is on the standart method
+// If we dont use we need to repeat the commands to refers to the standart method as exampple.
+//  std::cout << "Hello World" << std::endl; 
+// If the namespace is not used, then the computer finds for the cout, cin and enld cannot identify those.
+
+// This is a function, always need a entry point, it will be the main function. 
+int main() {
+    
+    // This line prints the Hello World 
+    cout << "Hello World" << endl;
+
+    // As the C++ is a Freeform Programming we could write the Hello World function on this way:
+    // cout << "Hello" << endl;
+    // cout << "World" << endl; 
+
+    return 0;
+}
