0facd3793832da64ba2bd0439b8f38e76cef4170
Ivan Junior <ivanssouzajr@gmail.com>
2022-10-07 06:59:45 +0900

    Data Structure and Data Type

---
+++ Logica de Programacao/variablesDataStructures.cpp
@@ -0,0 +1,35 @@
+//---------------------------------------------------------------------
+//  First rule on C++ is  to declare the variable name and the type.
+//  The primitive data type are:
+//
+//  int age, ammountOfJobs;
+//  float wage;
+//  double pi = 3.14151697
+//  bool likeCars = false; 
+//  
+//  For the variables names you can't use a number "2wage" to name a similar variable.
+//  But you can use after the variable name "wage2". 
+//---------------------------------------------------------------------
+
+#include <iostream>
+using namespace std;
+
+int main () {
+
+// int - inteiro de 32 bits
+    int a = 1;
+
+// long long - inteiro de 64 bits
+    long long a = 8000000000;
+
+// float - real de 32 bits
+    float x = 3.25;
+
+// double - real de 64 bits
+    double y = 3.25364758697014;
+
+// char - character de 8 bits
+    char leter = 'a';
+
+    return 0;
+}
