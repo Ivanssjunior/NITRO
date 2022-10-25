5e4caa0348d4b51ddcad73736fab317b7163d824
Ivan Junior <ivanssouzajr@gmail.com>
2022-10-25 16:59:28 +0900

    Infinite Loops Study

---
+++ Logica de Programacao/infiniteForLoop.cpp
@@ -0,0 +1,31 @@
+/*---------------------------------------------------------------------
+   Infinite For Loops
+    
+    A Infinite For Loop run indefinititely, when the condition in for statement is true whenever it is evaluated.
+
+    As the condition is never going to be false, the control never comes out of the loop and forms an Infinite Loop,
+    The condition will have a Inicialization going thru a condition and it will be verified if it's true,
+    After the being validated whith a true condition it will go thru the statements, and there we're are,
+    When the Statements are true the loop just keep updating, and it will never stops until the condition be false.
+    
+-----------------------------------------------------------------------*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+
+
+   
+   /*
+    For this Boolean expressiont instead provinding a expression we can just set the value true.
+
+        for ( ; true;) {
+            cout << "Hello" << endl; 
+        }
+   */
+    
+
+    return 0;
+}
