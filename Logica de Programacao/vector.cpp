a2f430f2257347364e85dc46ce783a835523e237
Ivan Junior <ivanssouzajr@gmail.com>
2022-10-31 16:27:09 +0900

    Vector Study

--- Logica de Programacao/vector.cpp
+++ Logica de Programacao/vector.cpp
@@ -3,60 +3,23 @@ using namespace std;
 
 int main() {
 
-    cout << "How many students has in your class? " << endl; 
+    int tamanho;
 
-    int n;
-    cin >> n;
+    cout << "How many scores you want to type?" << endl;
+    cin >> tamanho;
 
-    int grades[n];
-    int sum = 0;
+    float notas[tamanho]; 
 
-    for (int i=0; i < n; i++) {
-        cin >> grades[i];
-        sum += grades[i];
+    cout << "Now type the scores" << endl;
+    for (int i=0; i < tamanho; i++){
+        cin >> notas[i];
     }
 
-    float average =  sum / n;
-
-    cout << "Do you want to calculate the average? " << endl;
-    cin >> n;
-
-    if (n==1) {
-        cout << average << endl;
-    } else {
-        cout << "You choosse to not calculate the average score" << endl; 
+    cout << "You typed: ";
+    for (int i=0; i < tamanho; i++) {
+        cout << notas[i] << " ";
     }
-
-    // int tamanho;
-
-    // int n;
-
-    // cout << "How many grades would you like to store? " << endl; 
-
-    // cin >> tamanho; 
-
-    // float grades[tamanho];
-
-    // cout << "Now type the grades " << endl; 
-    // for (int i=0; i < tamanho; i++) {
-    //     cin >> grades[i];
-    // }
-
-    // cout << "You typed: ";
-    // for (int i=0; i < tamanho; i++) {
-    //     cout << grades[i] << " ";
-    // }
-
-    // cout << "Would you like to calculate the average? " << endl;
-    // cin >> n; 
-
-    // if (n==1) {
-    //     cout << sum[grades] << endl; 
-    // } else {
-    //     cout << "You choose to not calculate the average sum." << endl; 
-    // }
-
-    // cout << endl; 
+    cout << endl; 
 
     return 0;
 }
